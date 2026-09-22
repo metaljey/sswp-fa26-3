a8c7ccf30fb9a19e4cf233e0b563e553a98c1212
Madalene Nelson <madalene.nelson@hawkeyecollege.edu>
2026-09-22 15:46:55 -0500

    set up

---
+++ sswp-fa26-3/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sswp_fa26_3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
